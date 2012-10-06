49b769389bfa0e10e3de4fa75e3f3d7746f9d55b
root <pthreads@pthreads.org>
2012-10-06 18:38:56 +0200

    no pthreads handlers on imports for now fixup a couple of examples

--- examples/AdvancedSynchronization.php
+++ examples/AdvancedSynchronization.php
@@ -18,12 +18,13 @@ class ScopeTest2 extends Thread {
 		printf("%s: %lu running\n", __CLASS__, $this->getThreadId());
 		if (($other = Thread::getThread($this->other))) {
 			printf("%s: %lu working ... %s\n", __CLASS__, $this->getThreadId(), $other->fetch("my"));
+			printf("%s: %lu testing ... %s\n", __CLASS__, $this->getThreadId(), $other->my);
 			usleep(1000000); /* simulate some work */
 			if ($other->isWaiting())
 				printf("%s: %lu notifying %lu: %d\n", __CLASS__, $this->getThreadId(), $this->other, $other->notify());
 		} else {
 			printf("%s: %lu failed to find %lu\n", __CLASS__, $this->getThreadId(), $this->other);
-		};
+		}
 		printf("%s: %lu notified: %d\n", __CLASS__, $this->getThreadId(), $this->wait());
 	}
 }
@@ -36,8 +37,10 @@ $test2->start();
 
 printf("Process: notifying %lu: %d\n", $test2->getThreadId(), $test2->notify());
 
-/*
-* If importing has been disabled then the responsability to notify the waiting thread must fall back to the Process to avoid deadlock
-*/
-printf("Process: notifying %lu: %d\n", $test->getThreadId(), $test->notify());
+if ($test->isWaiting()) {
+	/*
+	* If importing has been disabled then the responsability to notify the waiting thread must fall back to the Process to avoid deadlock
+	*/
+	printf("Process: notifying %lu: %d\n", $test->getThreadId(), $test->notify());
+}
 ?>
