01d87986a0f472fc8c4ddd481cafa4ffa218e36d
krakjoe <joe.watkins@live.co.uk>
2013-10-08 06:40:40 +0100

    tidy

--- src/thread.h
+++ src/thread.h
@@ -165,7 +165,7 @@ static inline int pthreads_equal_func(void **first, void **second){
 } /* }}} */
 
 /* {{{ option constants */
-#define PTHREADS_INHERIT_NONE 0
+#define PTHREADS_INHERIT_NONE      0x00000000
 #define PTHREADS_INHERIT_INI       0x00000001
 #define PTHREADS_INHERIT_CONSTANTS 0x00000010
 #define PTHREADS_INHERIT_FUNCTIONS 0x00000100
@@ -175,26 +175,25 @@ static inline int pthreads_equal_func(void **first, void **second){
 #define PTHREADS_INHERIT_ALL       0x00111111 /* }}} */
 
 /* {{{ scope constants */
-#define PTHREADS_SCOPE_UNKNOWN 0
-#define PTHREADS_SCOPE_THREAD 1
-#define PTHREADS_SCOPE_WORKER 2
-#define PTHREADS_SCOPE_STACKABLE 4
-#define PTHREADS_SCOPE_CONNECTION 8
-#define PTHREADS_SCOPE_DETACHED 16
-/* }}} */
+#define PTHREADS_SCOPE_UNKNOWN     0x00000000
+#define PTHREADS_SCOPE_THREAD      0x00000001
+#define PTHREADS_SCOPE_WORKER      0x00000010
+#define PTHREADS_SCOPE_STACKABLE   0x00000100
+#define PTHREADS_SCOPE_CONNECTION  0x00001000
+#define PTHREADS_SCOPE_DETACHED    0x00010000 /* }}} */
 
 /* {{{ scope macros */
-#define PTHREADS_IS_KNOWN_ENTRY(t) (((t)->scope & PTHREADS_SCOPE_UNKNOWN)!=PTHREADS_SCOPE_UNKNOWN)
-#define PTHREADS_IS_CONNECTION(t) (((t)->scope & PTHREADS_SCOPE_CONNECTION)==PTHREADS_SCOPE_CONNECTION)
-#define PTHREADS_IS_NOT_CONNECTION(t) (((t)->scope & PTHREADS_SCOPE_CONNECTION)!=PTHREADS_SCOPE_CONNECTION)
-#define PTHREADS_IS_THREAD(t) (((t)->scope & PTHREADS_SCOPE_THREAD)==PTHREADS_SCOPE_THREAD)
-#define PTHREADS_IS_NOT_THREAD(t) (((t)->scope & PTHREADS_SCOPE_THREAD)!=PTHREADS_SCOPE_THREAD)
-#define PTHREADS_IS_WORKER(t) (((t)->scope & PTHREADS_SCOPE_WORKER)==PTHREADS_SCOPE_WORKER)
-#define PTHREADS_IS_NOT_WORKER(t) (((t)->scope & PTHREADS_SCOPE_WORKER)!=PTHREADS_SCOPE_WORKER)
-#define PTHREADS_IS_STACKABLE(t) (((t)->scope & PTHREADS_SCOPE_STACKABLE)==PTHREADS_SCOPE_STACKABLE)
-#define PTHREADS_IS_NOT_STACKABLE(t) (((t)->scope & PTHREADS_SCOPE_STACKABLE)!=PTHREADS_SCOPE_STACKABLE)
-#define PTHREADS_IS_DETACHED(t) (((t)->scope & PTHREADS_SCOPE_DETACHED)==PTHREADS_SCOPE_DETACHED)
-#define PTHREADS_IS_NOT_DETACHED(t)  (((t)->scope & PTHREADS_SCOPE_DETACHED)!=PTHREADS_SCOPE_DETACHED)
+#define PTHREADS_IS_KNOWN_ENTRY(t)      ((t)->scope)
+#define PTHREADS_IS_CONNECTION(t)       ((t)->scope & PTHREADS_SCOPE_CONNECTION)
+#define PTHREADS_IS_NOT_CONNECTION(t)   (!PTHREADS_IS_CONNECTION(t))
+#define PTHREADS_IS_THREAD(t)           ((t)->scope & PTHREADS_SCOPE_THREAD)
+#define PTHREADS_IS_NOT_THREAD(t)       (!PTHREADS_IS_THREAD(t))
+#define PTHREADS_IS_WORKER(t)           ((t)->scope & PTHREADS_SCOPE_WORKER)
+#define PTHREADS_IS_NOT_WORKER(t)       (!PTHREADS_IS_WORKER(t))
+#define PTHREADS_IS_STACKABLE(t)        ((t)->scope & PTHREADS_SCOPE_STACKABLE)
+#define PTHREADS_IS_NOT_STACKABLE(t)    (!PTHREADS_IS_STACKABLE(t))
+#define PTHREADS_IS_DETACHED(t)         ((t)->scope & PTHREADS_SCOPE_DETACHED)
+#define PTHREADS_IS_NOT_DETACHED(t)     (!PTHREADS_IS_DETACHED(t))
 /* }}} */
 
 /* {{{ pthread_self wrapper */
@@ -205,41 +204,6 @@ static inline ulong pthreads_self() {
 	return (ulong) pthread_self();
 #endif
 } /* }}} */
-
-/* {{{ mode enumeration for string length function */
-typedef enum {
-    PTHREADS_STRLEN_T,
-    PTHREADS_STRLEN_NT
-} pthreads_strlen_mode_t; /* }}} */
-
-/* {{{ calculate/ascertain or request length of null terminated or not null terminated string */
-static inline size_t pthreads_strlen(const char* str, size_t length, pthreads_strlen_mode_t mode) {
-    switch (mode) {
-        case PTHREADS_STRLEN_T: switch (str[length+1]) {
-            case '\0':
-                php_printf("(2)found null at %d, return %d\n", length, length+1);
-                return length+1;
-                
-            default: switch (str[length]) {
-                case '\0': 
-                    php_printf("(3)found null at %d+1, return %d\n", length, length+1);
-                    return length;
-                    
-                default: return strlen(str) + 1;
-            }
-        } break;
-        
-        case PTHREADS_STRLEN_NT: {
-            
-        } break;
-    }
-} /* }}} */
-
-/* {{{ get null terminated string length */
-#define PTHREADS_STRLEN(s, l) \
-    (((s)[(l)] == '\0') ? (l) : \
-        ((s)[(l)-1] == '\0') ? ((l)+1) : \
-            (strlen((s)))) /* }}} */
 
 /* {{{ tell if the calling thread created referenced PTHREAD */
 #define PTHREADS_IN_CREATOR(t)	(t->cls == tsrm_ls) /* }}} */
