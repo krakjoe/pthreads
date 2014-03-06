2f0ff563a9e2925ae8aa17dd744b6021e36b68cc
krakjoe <joe.watkins@live.co.uk>
2014-03-06 18:28:02 +0000

    update copyright years/info

--- php_pthreads.h
+++ php_pthreads.h
@@ -2,7 +2,7 @@
   +----------------------------------------------------------------------+
   | pthreads                                                             |
   +----------------------------------------------------------------------+
-  | Copyright (c) Joe Watkins 2012                               		 |
+  | Copyright (c) Joe Watkins 2012 - 2014                                |
   +----------------------------------------------------------------------+
   | This source file is subject to version 3.01 of the PHP license,      |
   | that is bundled with this package in the file LICENSE, and is        |
