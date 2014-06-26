2c9ce71611024bcb0da82c5775c3fdf6243b4158
John Wellesz <John.Wellesz@teaser.fr>
2014-06-26 04:41:09 +0200

    fix #323 where ktype was no longer correctly set making chunk() key preservation fail.

--- src/store.c
+++ src/store.c
@@ -303,10 +303,10 @@ int pthreads_store_chunk(zval *object, long size, zend_bool preserve, zval **chu
             ALLOC_INIT_ZVAL(member);
             
             pthreads_store_convert(storage, member TSRMLS_CC);
-            
-            ktype = zend_hash_get_current_key_ex(table, &key, &klen, &idx, 0, &position) == HASH_KEY_IS_STRING ?
-            	HASH_DEL_KEY : HASH_DEL_INDEX;
-            zend_hash_del_key_or_index(table, key, klen, idx, ktype);
+           
+            ktype = zend_hash_get_current_key_ex(table, &key, &klen, &idx, 0, &position);
+            zend_hash_del_key_or_index(table, key, klen, idx, ktype == HASH_KEY_IS_STRING ?
+            	HASH_DEL_KEY : HASH_DEL_INDEX);
             
             if (!preserve) {
                 zend_hash_next_index_insert(
