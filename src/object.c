krakjoe/pthreads

commit 398304064b6482f0a4077d7238750114c8e42ab5

implement closures as members

src/store.c
@@ -34,6 +34,10 @@
 #	include <src/resources.h>
 #endif
 
+#ifndef HAVE_PTHREADS_COPY_H
+#	include <src/copy.h>
+#endif
+
 typedef struct _pthreads_storage {
 	zend_uchar 	type;
 	size_t 		length;
@@ -516,6 +520,15 @@ static void pthreads_store_create(pthreads_storage *storage, zval *unstore, zend
 			break;
 			
 			case IS_OBJECT:
+			    if (instanceof_function(Z_OBJCE_P(unstore), zend_ce_closure TSRMLS_CC)) {
+			        const zend_function *def = 
+			            zend_get_closure_method_def(unstore TSRMLS_CC);
+			        storage->type = IS_CLOSURE;
+			        storage->data = (zend_function*) malloc(sizeof(zend_function));
+			        memcpy(storage->data, def, sizeof(zend_function));
+			        break;
+			    }
+			    
 			case IS_ARRAY: {
 				if (pthreads_store_tostring(unstore, (char**) &storage->data, &storage->length, complex TSRMLS_CC)==SUCCESS) {
 					if (storage->type==IS_ARRAY) {
@@ -599,6 +612,30 @@ static int pthreads_store_convert(pthreads_storage *storage, zval *pzval TSRMLS_
 					zend_list_addref(Z_RESVAL_P(pzval));
 				}
 			} break;
+			
+			case IS_CLOSURE: {
+			    char *name;
+			    int name_len;
+			    
+			    zend_function *closure = (zend_function*) storage->data;
+                
+                pthreads_copy_function(closure);
+
+			    zend_create_closure(
+			        pzval, closure, EG(scope), EG(This) TSRMLS_CC);
+			    
+			    closure = (zend_function*) zend_get_closure_method_def(pzval TSRMLS_CC);
+			    
+			    name_len = spprintf(&name, 0, "Closure@%p", closure);
+			    
+			    zend_hash_update(
+			        EG(function_table), name, name_len, (void**)closure, sizeof(zend_function), NULL);
+			    
+			    result = SUCCESS;
+			    
+			    efree(name);
+			} break;
+			
 			case IS_ARRAY:
 			case IS_OBJECT: {
 				result = pthreads_store_tozval(
@@ -902,6 +939,7 @@ static int pthreads_store_remove_complex_recursive(zval **pzval TSRMLS_DC) {
 static void pthreads_store_storage_dtor (pthreads_storage *storage){
 	if (storage) {
 		switch (storage->type) {
+		    case IS_CLOSURE:
 			case IS_OBJECT:
 			case IS_STRING:
 			case IS_ARRAY:

src/store.h
@@ -30,6 +30,8 @@
 #	include <src/lock.h>
 #endif
 
+#define IS_CLOSURE IS_CALLABLE
+
 /* {{{ buffer structure */
 typedef struct _pthreads_store {
 	HashTable         table;

tests/closures-as-members.phpt
@@ -1,0 +1,67 @@
+--TEST--
+Testing closure members
+--DESCRIPTION--
+This test verifies that closures can be set as members and called from anywhere
+--FILE--
+<?php
+$test = new Threaded();
+
+$test->some = function(){
+    echo "Hello Some\n";
+};
+
+class T extends Thread {
+
+    public function __construct($test) {
+        $this->test = $test;
+    }
+    
+    public function run() {
+        /* call original closure */
+        $this->call($this->test->some);
+        
+        /* set new closure */
+        $this->synchronized(function(){
+            $this->set = true;
+            $this->test->some = function() {
+                echo "Hello World\n";
+            };
+            $this->notify();
+        });
+        
+        /* wait for new closure execution */
+        $this->synchronized(function(){
+            if (!$this->used)
+                $this->wait();
+        });
+    }
+    
+    public function call(Closure $closure) {
+        $closure();
+    }
+}
+
+/* start thread to call closure */
+$t = new T($test);
+$t->start();
+
+/* wait for new closure */
+$t->synchronized(function() use($t) {
+    if (!$t->set)
+        $t->wait();
+});
+
+/* call new closure */
+$some = $test->some;
+$some();
+
+/* allow new closure to be dtored with old context */
+$t->synchronized(function() use($t) {
+    $t->used = true;
+    $t->notify();
+});
+
+$t->join();
+--EXPECTF--
+Hello Some
+Hello World

commit 7a95e46f6e9099bd878824348f133dbfd863e154

fix 5.3 compat, broken by 398304064b6482f0a4077d7238750114c8e42ab5

src/object.c
@@ -486,8 +486,7 @@ static inline void pthreads_base_init(PTHREAD base TSRMLS_DC) {
 				(char**)&class_name,
 				(char**)&property_name);
 			property_name_len = strlen(property_name);
-			pthreads_store_write(base->store, property_name, property_name_len, property TSRMLS_CC);
-			
+			pthreads_store_write(base->store, (char*) property_name, property_name_len, property TSRMLS_CC);
 		}
 	}
 #else

src/store.c
@@ -619,8 +619,11 @@ static int pthreads_store_convert(pthreads_storage *storage, zval *pzval TSRMLS_
                 
                 pthreads_copy_function(closure);
 
-			    zend_create_closure(
-			        pzval, closure, EG(scope), EG(This) TSRMLS_CC);
+#if PHP_VERSION_ID >= 50400
+                zend_create_closure(pzval, closure, EG(scope), EG(This) TSRMLS_CC);
+#else
+                zend_create_closure(pzval, closure TSRMLS_CC);
+#endif
 			    
 			    closure = (zend_function*) zend_get_closure_method_def(pzval TSRMLS_CC);
 			    

src/store.h
@@ -30,6 +30,10 @@
 #	include <src/lock.h>
 #endif
 
+#ifndef IS_CALLABLE
+# define IS_CALLABLE 10
+#endif
+
 #define IS_CLOSURE IS_CALLABLE
 
 /* {{{ buffer structure */

tests/closures-as-members.phpt
@@ -21,19 +21,19 @@ class T extends Thread {
         $this->call($this->test->some);
         
         /* set new closure */
-        $this->synchronized(function(){
-            $this->set = true;
-            $this->test->some = function() {
+        $this->synchronized(function($thread){
+            $thread->set = true;
+            $thread->test->some = function() {
                 echo "Hello World\n";
             };
-            $this->notify();
-        });
+            $thread->notify();
+        }, $this);
         
         /* wait for new closure execution */
-        $this->synchronized(function(){
-            while (!$this->used)
-                $this->wait();
-        });
+        $this->synchronized(function($thread){
+            while (!$thread->used)
+                $thread->wait();
+        }, $this);
     }
     
     public function call(Closure $closure) {

tests/objects-from-closures.phpt
@@ -24,7 +24,7 @@ $threaded = Thread::from(function() {
     var_dump($this->test);
 }, function($test) {
     $this->test = $test;
-}, [$test]);
+}, array($test));
 
 $threaded->start();
 $threaded->join();