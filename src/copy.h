c1352ce75c11aa028943915f9cf621c0a211ad5f
Thomas Punt <tpunt@hotmail.co.uk>
2017-10-22 21:43:03 +0100

    Fix anonymous classes for 32bit builds

--- src/copy.h
+++ src/copy.h
@@ -154,6 +154,7 @@ static zend_op* pthreads_copy_opcodes(zend_op_array *op_array, zval *literals) {
 
 	memcpy(copy, op_array->opcodes, sizeof(zend_op) * op_array->last);
 
+	/* The following code comes from ext/opcache/zend_persist.c */
 #if ZEND_USE_ABS_CONST_ADDR || ZEND_USE_ABS_JMP_ADDR
 	zend_op *opline = copy;
 	zend_op *end    = copy + op_array->last;
@@ -166,28 +167,35 @@ static zend_op* pthreads_copy_opcodes(zend_op_array *op_array, zval *literals) {
 			opline->op2.zv = (zval*)((char*)opline->op2.zv + ((char*)op_array->literals - (char*)literals));
 #endif
 #if ZEND_USE_ABS_JMP_ADDR
-		if ((op_array->fn_flags & ZEND_ACC_DONE_PASS_TWO) != 0) {
+		if (op_array->fn_flags & ZEND_ACC_DONE_PASS_TWO) {
+			/* fix jumps to point to new array */
 			switch (opline->opcode) {
 				case ZEND_JMP:
 				case ZEND_FAST_CALL:
-				case ZEND_DECLARE_ANON_CLASS:
-				case ZEND_DECLARE_ANON_INHERITED_CLASS:
-					 opline->op1.jmp_addr = &copy[opline->op1.jmp_addr - op_array->opcodes];
-				break;
-
+					opline->op1.jmp_addr = &copy[opline->op1.jmp_addr - op_array->opcodes];
+					break;
 				case ZEND_JMPZNZ:
+					/* relative extended_value don't have to be changed */
+					/* break omitted intentionally */
 				case ZEND_JMPZ:
 				case ZEND_JMPNZ:
 				case ZEND_JMPZ_EX:
 				case ZEND_JMPNZ_EX:
 				case ZEND_JMP_SET:
 				case ZEND_COALESCE:
-				case ZEND_NEW:
 				case ZEND_FE_RESET_R:
 				case ZEND_FE_RESET_RW:
 				case ZEND_ASSERT_CHECK:
 					opline->op2.jmp_addr = &copy[opline->op2.jmp_addr - op_array->opcodes];
-				break;
+					break;
+				case ZEND_DECLARE_ANON_CLASS:
+				case ZEND_DECLARE_ANON_INHERITED_CLASS:
+				case ZEND_FE_FETCH_R:
+				case ZEND_FE_FETCH_RW:
+				case ZEND_SWITCH_LONG:
+				case ZEND_SWITCH_STRING:
+					/* relative extended_value don't have to be changed */
+					break;
 			}
 		}
 #endif
@@ -208,6 +216,7 @@ static zend_op* pthreads_copy_opcodes(zend_op_array *op_array, zval *literals) {
 
 	memcpy(copy, op_array->opcodes, sizeof(zend_op) * op_array->last);
 
+	/* The following code comes from ext/opcache/zend_persist.c */
 	zend_op *opline = copy;
 	zend_op *end    = copy + op_array->last;
 
@@ -236,28 +245,35 @@ static zend_op* pthreads_copy_opcodes(zend_op_array *op_array, zval *literals) {
 			}
 #endif
 #if ZEND_USE_ABS_JMP_ADDR
-		if ((op_array->fn_flags & ZEND_ACC_DONE_PASS_TWO) != 0) {
+		if (op_array->fn_flags & ZEND_ACC_DONE_PASS_TWO) {
+			/* fix jumps to point to new array */
 			switch (opline->opcode) {
 				case ZEND_JMP:
 				case ZEND_FAST_CALL:
-				case ZEND_DECLARE_ANON_CLASS:
-				case ZEND_DECLARE_ANON_INHERITED_CLASS:
-					 opline->op1.jmp_addr = &copy[opline->op1.jmp_addr - op_array->opcodes];
-				break;
-
+					opline->op1.jmp_addr = &copy[opline->op1.jmp_addr - op_array->opcodes];
+					break;
 				case ZEND_JMPZNZ:
+					/* relative extended_value don't have to be changed */
+					/* break omitted intentionally */
 				case ZEND_JMPZ:
 				case ZEND_JMPNZ:
 				case ZEND_JMPZ_EX:
 				case ZEND_JMPNZ_EX:
 				case ZEND_JMP_SET:
 				case ZEND_COALESCE:
-				case ZEND_NEW:
 				case ZEND_FE_RESET_R:
 				case ZEND_FE_RESET_RW:
 				case ZEND_ASSERT_CHECK:
 					opline->op2.jmp_addr = &copy[opline->op2.jmp_addr - op_array->opcodes];
-				break;
+					break;
+				case ZEND_DECLARE_ANON_CLASS:
+				case ZEND_DECLARE_ANON_INHERITED_CLASS:
+				case ZEND_FE_FETCH_R:
+				case ZEND_FE_FETCH_RW:
+				case ZEND_SWITCH_LONG:
+				case ZEND_SWITCH_STRING:
+					/* relative extended_value don't have to be changed */
+					break;
 			}
 		}
 #endif
