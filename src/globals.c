krakjoe/pthreads

commit edf81d3cc0f34c1fcb3f8f14435a9a122f6e482f

fix 7.2 compile

src/globals.c
@@ -47,10 +47,18 @@ zend_bool pthreads_globals_init(){
 		    	&PTHREADS_G(objects), 64, NULL, (dtor_func_t) NULL, 1);
 		}
 
+#if PHP_VERSION_ID >= 70200
+#define INIT_STRING(n, v) do { \
+	PTHREADS_G(strings).n = zend_string_init(v, 1); \
+	GC_ADDREF(PTHREADS_G(strings).n); \
+} while(0)
+#else
 #define INIT_STRING(n, v) do { \
 	PTHREADS_G(strings).n = zend_string_init(v, 1); \
 	GC_REFCOUNT(PTHREADS_G(strings).n)++; \
 } while(0)
+#endif
+
 		INIT_STRING(run, ZEND_STRL("run"));
 		INIT_STRING(session.cache_limiter, ZEND_STRL("cache_limiter"));
 		INIT_STRING(session.use_cookies, ZEND_STRL("use_cookies"));

commit 4e296c542294ddc65bdb9c10ae6e1dd770a986e7

fix compile error on 7.2 caused by edf81d3cc0f34c1fcb3f8f14435a9a122f6e482f

src/globals.c
@@ -47,7 +47,7 @@ zend_bool pthreads_globals_init(){
 		    	&PTHREADS_G(objects), 64, NULL, (dtor_func_t) NULL, 1);
 		}
 
-#if PHP_VERSION_ID >= 70200
+#if PHP_VERSION_ID >= 70300
 #define INIT_STRING(n, v) do { \
 	PTHREADS_G(strings).n = zend_string_init(v, 1); \
 	GC_ADDREF(PTHREADS_G(strings).n); \