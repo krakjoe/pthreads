e25675dee989a2e15b81da6c0e215d22bacff86a
Bastian Schneider <b.schneider@badnoob.com>
2018-10-10 10:12:38 +0200

    Don't cache closures set as members in resolve HT (#827)
    * Don't cache closures set as members in resolve HT
    
    * removed \Collectable from TestClosure declaration

--- src/copy.h
+++ src/copy.h
@@ -380,17 +380,25 @@ static inline zend_function* pthreads_copy_internal_function(zend_function *func
 
 /* {{{ */
 static zend_function* pthreads_copy_function(zend_function *function) {
-	zend_function *copy = zend_hash_index_find_ptr(&PTHREADS_ZG(resolve), (zend_ulong)function);
-	
-	if (copy) {
-		function_add_ref(copy);
-		return copy;
+	zend_function *copy;
+
+	if (!(function->op_array.fn_flags & ZEND_ACC_CLOSURE)) {
+		copy = zend_hash_index_find_ptr(&PTHREADS_ZG(resolve), (zend_ulong)function);
+
+		if (copy) {
+			function_add_ref(copy);
+			return copy;
+		}
 	}
 	
 	if (function->type == ZEND_USER_FUNCTION) {
 		copy = pthreads_copy_user_function(function);
 	} else {
 		copy = pthreads_copy_internal_function(function);
+	}
+
+	if (function->op_array.fn_flags & ZEND_ACC_CLOSURE) { //don't cache closures
+		return copy;
 	}
 
 	return zend_hash_index_update_ptr(&PTHREADS_ZG(resolve), (zend_ulong) function, copy);
