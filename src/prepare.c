krakjoe/pthreads

commit de5d8f2c973890187ba23c6f7e7547e99b602fac

condition not necessary

src/prepare.c
@@ -614,11 +614,7 @@ static inline void pthreads_prepare_constants(pthreads_object_t* thread) {
 						} break;
 						case IS_DOUBLE: Z_DVAL(constant.value)=Z_DVAL(zconstant->value); break;
 						case IS_STRING: {
-#if PHP_VERSION_ID >= 70300
-							Z_STR(constant.value)= zend_string_new(Z_STR(zconstant->value));
-#else
-							ZVAL_NEW_STR(&constant.value, zend_string_new(Z_STR(zconstant->value)));
-#endif
+							Z_STR(constant.value) = zend_string_new(Z_STR(zconstant->value));
 						} break;
 						case IS_ARRAY: {
 							pthreads_store_separate(&zconstant->value, &constant.value, 1);

commit 5c29ef72a448989b53bcbff03dc5160705aa4fd7

String constants created by define() are not always interned
A premature optimization was introduced to constant string copying in a473944746b778b41c70b7fb25037dd97e10ff0d and worsened in de5d8f2c973890187ba23c6f7e7547e99b602fac, which caused faults with string copying of constants created using define().

For example, [this string](https://github.com/pmmp/PocketMine-MP/blob/adb78679c56f06a33f8ca1bd1f7fbf903af6fa5d/src/pocketmine/level/format/SubChunk.php#L37) is allocated using str_repeat() and is not interned. This caused faults later on when modifying a copy of this string assigned to a variable (copy on write issues).

src/prepare.c
@@ -614,7 +614,7 @@ static inline void pthreads_prepare_constants(pthreads_object_t* thread) {
 						} break;
 						case IS_DOUBLE: Z_DVAL(constant.value)=Z_DVAL(zconstant->value); break;
 						case IS_STRING: {
-							Z_STR(constant.value) = zend_string_new(Z_STR(zconstant->value));
+							ZVAL_NEW_STR(&constant.value, zend_string_new(Z_STR(zconstant->value)));
 						} break;
 						case IS_ARRAY: {
 							pthreads_store_separate(&zconstant->value, &constant.value, 1);